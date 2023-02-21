4441401af381f7d57057d2a09a5e70df25d0b589
clint360 <clint360.rebase@gmail.com>
2023-02-21 16:31:47 +0100

    Progress with URLs

--- add.js
+++
@@ -1,8 +0,0 @@
-function add(a, b){
-  return a + b
-}
-function add10(a) {
-  return a + 10;
-}
-
-module.exports = (add, add10);
--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const fs = require('fs');
+const url = require('url');
 const http = require('http');
 
+function extension(val) {
+  return (val === 'js') ? `script/${val.split('.').pop()}`
+   :
+    `text/${val.split('.').pop()}`;
+}
+
 http.createServer((req, res) => {
-  fs.readFile(__dirname + req.url, (err, data) => {
-    console.log(__dirname)
+  let requestPath = 'static' + req.url;
+  let urlpath = url.parse(req.url, true);
+  urlpath = urlpath.pathname
+  console.log(urlpath)
+  if (urlpath === '/') {
+    fs.readFile('static/index.html', (err, data) => {
+      if (err) {} else {
+      res.writeHead(200, { 'Content-Type': extension(urlpath) });
+      res.end(data);
+    }}
+    )
+  } else {
+
+  fs.readFile(requestPath, (err, data) => {
     if (err) {
-      res.writeHead(404, { 'Content-Type': 'text/html' });
-      res.end('404: File not found');
+        fs.readFile('static/error.html', (err, data) => {
+          if (err) {} else {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end(data);
+        }}
+        )
     } else {
       res.writeHead(200, { 'Content-Type': 'text/html' });
       res.end(data);
     }
   });
-}).listen(8000);
+}}).listen(8000);
